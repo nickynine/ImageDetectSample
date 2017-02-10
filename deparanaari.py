4721b69a7180c7bf82d907fbcc98bd25f63fccf2
nickynine <thushar.gr@gmail.com>
2017-02-10 17:55:36 +0530

    circle rectangle line polyline and text in opencv

--- deparanaari.py
+++ deparanaari.py
@@ -45,8 +45,26 @@ def markWatchImage():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+def imageOperations():
+    img = cv2.imread('watch.jpg', cv2.IMREAD_COLOR)
+    img[55,55] =[0,0,255]
+
+    roi = img[100:150,100:150]  = [255,255,255]
+    watch_face = img[37:111,107:194]
+    img[0:74,0:87] = watch_face
+
+
+
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 
 def intuit():
     print("hi There")
 intuit()
-markWatchImage()
+#scan()
+#markWatchImage()
+#imageOperations()
+
